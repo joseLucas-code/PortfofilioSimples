6c51efab4245399a49181d2105c0937c12da7c74
joseLucas-code <78504465+joseLucas-code@users.noreply.github.com>
2021-02-08 22:15:43 -0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,111 @@
+// menu mobile
+
+const menuMobile = document.querySelector('i.btnMenu')
+
+menuMobile.addEventListener('click', ()=>{
+    const menu = document.querySelector('header nav')
+
+    menu.classList.toggle('menuOpen')
+
+    document.body.classList.toggle('overflowHidden')
+})
+
+//slide 
+
+const slideContent = document.querySelectorAll('.slide-content')
+const dots = document.querySelectorAll('.single-dot')
+const prevBTN = document.querySelector('.prevBTN')
+const nextBTN = document.querySelector('.nextBTN')
+const totalSlides = slideContent.length - 1;
+let currentSlideIndex = 0;
+
+nextBTN.addEventListener('click', () =>{
+    currentSlideIndex === totalSlides ? currentSlideIndex = 0 : currentSlideIndex++;
+
+    mudarSlide();
+})
+
+prevBTN.addEventListener('click', () =>{
+    currentSlideIndex === 0 ? currentSlideIndex = totalSlides : currentSlideIndex--;
+
+    mudarSlide();
+})
+
+dots.forEach((dot,indexDot) =>{
+    dot.addEventListener('click', () =>{
+        currentSlideIndex = indexDot
+
+        mudarSlide();
+    })
+
+    
+})
+
+function mudarSlide(){
+
+    slideContent.forEach(slide => slide.classList.remove('activeSlide'))
+    slideContent[currentSlideIndex].classList.add('activeSlide')
+
+    dots.forEach(dot => dot.classList.remove('activeDot'))
+    dots[currentSlideIndex].classList.add('activeDot')
+}
+
+setInterval(function(){
+    currentSlideIndex === totalSlides ? currentSlideIndex = 0 : currentSlideIndex++;
+
+    mudarSlide();
+},5000)
+
+// scrollTo navigation
+
+const arrowDown = document.querySelectorAll('.arrow-down a')
+const LinkMenu = document.querySelectorAll('header nav ul li a')
+
+
+LinkMenu.forEach((linkMenu, indexLink) => {
+    linkMenu.addEventListener('click', () => {
+
+        if(indexLink > 0){
+            let arrowDownAttribute = LinkMenu[indexLink].getAttribute('data-scrollTO')
+            scrollWin(arrowDownAttribute)
+        }
+
+    })
+})
+
+arrowDown.forEach((arrow, indexArrow) => {
+    arrow.addEventListener('click', (e) => {
+
+        let arrowDownAttribute = arrowDown[indexArrow].getAttribute('data-scrollTO')
+        scrollWin(arrowDownAttribute)
+
+    })
+})
+
+function scrollWin(el){
+    let scrollID = document.querySelector(el)
+
+    scrollID.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    })
+
+}
+
+//SCROLL TO TOP 
+
+
+
+window.addEventListener('scroll', ()=>{
+    const scrollUP = document.querySelector('.scroll-up')
+    scrollUP.classList.toggle('scrollDisplay', window.scrollY > 250)
+
+    scrollUP.addEventListener('click', ()=>{
+        document.body.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+})
+
+
